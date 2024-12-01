fa0e8ddf060e5033aad6f19030203690d11c7d08
Rene kuhm <renekuhm2@gmail.com>
2024-12-01 11:19:29 -0300

    se cambio supabase por appwrite 5

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -4,6 +4,7 @@ const client = new Client();
 client
   .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // Your API Endpoint
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // Your project ID
+  //.setKey(import.meta.env.VITE_APPWRITE_API_KEY);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
