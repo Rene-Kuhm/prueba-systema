6b3fb4b71d2df7ac5902d53d3af2585a9af736a4
Rene kuhm <renekuhm2@gmail.com>
2024-12-01 11:32:53 -0300

    se cambio supabase por appwrite 7

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -3,7 +3,7 @@ import { Client, Account, Databases, Query } from 'appwrite';
 const client = new Client();
 client
   .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-  .setProject('674c3c7a0019c1239f5a'); // Your project ID
+  .setProject('674c72fa28eb0e59bde6'); // Your project ID
   //.setKey(import.meta.env.VITE_APPWRITE_API_KEY);
 
 export const account = new Account(client);
