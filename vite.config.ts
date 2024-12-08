7a0becf2e6780cdce1bc78afbdf458a543036398
Rene kuhm <renekuhm2@gmail.com>
2024-12-08 19:21:40 -0300

    se agrego notificasion 8

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       strategies: 'injectManifest',
-      srcDir: '@/sw.ts',
+      srcDir: '.',
       filename: 'sw.ts',
       injectRegister: 'auto',
       manifest: {
