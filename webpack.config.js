a11f6967d7f32f7c8febb025fdac68d841dd72c3
Rene kuhm <renekuhm2@gmail.com>
2024-12-09 11:33:49 -0300

    se agrego notificasion 21

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,11 @@ module.exports = {
       'process.env.PUBLIC_URL': JSON.stringify(process.env.PUBLIC_URL || ''),
     }),
   ],
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true, // Asegúrate de que las rutas se manejen correctamente
+    headers: {
+      'Content-Type': 'application/javascript', // Asegúrate de que el MIME type sea correcto
+    },
+  },
 };
