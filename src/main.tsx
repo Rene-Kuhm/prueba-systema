ad026dae97ffb46520a307f97222a4708635b0a4
Rene kuhm <renekuhm2@gmail.com>
2024-12-09 11:26:35 -0300

    se agrego notificasion 20

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import '@/styles/globals.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
 
-// Registrar el service worker
-serviceWorkerRegistration.register();
+  // Registrar el service worker
+  serviceWorkerRegistration.register();
+} else {
+  console.error('No se encontró el elemento con id "root"');
+}
