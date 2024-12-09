915c4f1cdb8d06be402a91edbcbfc7de16690864
Rene kuhm <renekuhm2@gmail.com>
2024-12-09 12:11:09 -0300

    se agrego notificasion 30

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/globals.css';
-//import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
@@ -16,4 +16,4 @@ root.render(
 
 // Registrar el service worker
 
-//serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
