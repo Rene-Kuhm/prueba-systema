e84a36baa556c63d0b0f052e83e333e4e2ce2606
Rene kuhm <renekuhm2@gmail.com>
2024-12-09 12:04:01 -0300

    se agrego notificasion 28}

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import '@/styles/globals.css';
+import './styles/globals.css';
 //import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
