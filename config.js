a481cd4b446ff9d391e148f76a2b72b34432d30b
Rene kuhm <renekuhm2@gmail.com>
2024-12-14 20:19:03 -0300

    se agrego pagina technician 10

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    firebase: {
+        projectId: 'cospecreclamos', // Replace with your actual Firebase project ID
+        functionUrl: 'https://us-central1-your-firebase-project-id.cloudfunctions.net/sendClaimNotification'
+    }
+};
+
+export default config;
