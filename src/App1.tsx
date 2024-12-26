b15e11808612857adcf773b00cc7407e6a04be2f
Rene kuhm <renekuhm2@gmail.com>
2024-12-26 13:40:36 -0300

    se mejoro performance 22

---
+++ src/App1.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-blue-600">Hola Mundo</h1>
+        <p className="mt-4 text-gray-700">¡Bienvenido a mi primera aplicación React!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
