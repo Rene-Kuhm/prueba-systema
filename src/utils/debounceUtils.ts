4859e94144f43c17d7d41959a10317d3694e4993
Rene kuhm <renekuhm2@gmail.com>
2024-12-28 19:10:52 -0300

    se mejoro chuncks 4

--- src/utils/debounceUtils.ts
+++ src/utils/debounceUtils.ts
@@ -1,20 +1,39 @@
-import type { DebouncedFunc } from 'lodash';
+import { debounce, throttle } from 'lodash';
 
 type AnyFunction = (...args: unknown[]) => unknown;
-type AsyncFunction = (...args: unknown[]) => Promise<unknown>;
 
-export const createDebounce = async <T extends AnyFunction>(
-  func: T,
-  wait: number
-): Promise<DebouncedFunc<T>> => {
-  const { debounce } = await import('lodash');
-  return debounce(func, wait);
+type DebouncedFn<T extends AnyFunction> = {
+  (...args: Parameters<T>): ReturnType<T>;
+  cancel: () => void;
+  flush: () => ReturnType<T>;
 };
 
-export const createThrottle = async <T extends AsyncFunction>(
+export const debouncedFunction = <T extends AnyFunction>(
   func: T,
   wait: number
-): Promise<DebouncedFunc<T>> => {
-  const { throttle } = await import('lodash');
-  return throttle(func, wait);
+): DebouncedFn<T> => debounce(func, wait) as unknown as DebouncedFn<T>;
+
+export const throttledFunction = <T extends AnyFunction>(
+  func: T,
+  wait: number
+): DebouncedFn<T> => throttle(func, wait) as unknown as DebouncedFn<T>;
+
+export const createDebounce = <T extends (...args: unknown[]) => Promise<unknown> | unknown>(
+  func: T,
+  wait: number
+): (() => Promise<Awaited<ReturnType<T>>>) => {
+  let timeout: NodeJS.Timeout | null = null;
+
+  return (): Promise<Awaited<ReturnType<T>>> => {
+    return new Promise((resolve) => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+
+      timeout = setTimeout(async () => {
+        const result = await Promise.resolve(func());
+        resolve(result as Awaited<ReturnType<T>>);
+      }, wait);
+    });
+  };
 };
