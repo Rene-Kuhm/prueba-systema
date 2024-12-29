a428ac6db2cd32708ab0fedd065fd1de975da830
Rene kuhm <renekuhm2@gmail.com>
2024-12-29 16:47:43 -0300

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,59 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
     },
   },
-  base: '/', 
   build: {
-    outDir: 'dist', 
-    assetsDir: 'assets', 
+    target: 'es2015',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ...splitVendorChunkPlugin(),
         },
         chunkFileNames: 'assets/js/[name]-[hash].js',
         entryFileNames: 'assets/js/[name]-[hash].js',
         assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
       }
     },
-    chunkSizeWarningLimit: 2000, 
+    chunkSizeWarningLimit: 2000,
+    reportCompressedSize: true,
+    cssCodeSplit: true,
   },
   optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'], //optimize
+    include: ['react', 'react-dom', 'react-router-dom'],
+    exclude: ['your-local-package'],
   },
+  server: {
+    cors: true,
+    proxy: {},
+  }
 });
+
+function splitVendorChunkPlugin() {
+  return {
+    utils: ['lodash', 'moment'],
+    ui: ['@mui/material', '@emotion/react'],
+  };
+}
