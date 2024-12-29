038bea88f018bbd8700bb2b329f0fa244f933e27
Rene kuhm <renekuhm2@gmail.com>
2024-12-29 12:07:50 -0300

    se mejoro chuncks 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
-    },
-  },
 })
